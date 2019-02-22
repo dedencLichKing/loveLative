2cb0a25ffcbdee2faf33fe281792386d0871f7ef
dedencLichKing <165057345@qq.com>
2019-02-22 13:37:16 +0800

    Update spot.js
    1.commit

--- spot.js
+++ spot.js
@@ -10,23 +10,7 @@
     start();
     function start() {
         document.getElementById('go').removeEventListener('click', start);
-        promise.then(() => engine.toText('Hi你好呀'))
-        .then(() => engine.shake())
-        .then(() => engine.toText('真的'))
-        .then(() => engine.shake())
-        .then(() => engine.toText('对不起'))
-        .then(() => engine.shake())
-	    .then(() => engine.toText('让你'))
-        .then(() => engine.shake())
-        .then(() => engine.toText('生气啦'))
-        .then(() => engine.shake())
-        .then(() => engine.toText('给你'))
-        .then(() => engine.shake())
-        .then(() => engine.toText('道歉啦'))
-        .then(() => engine.shake())
-        .then(() => engine.toText('我写的'))
-        .then(() => engine.shake())
-        .then(() => engine.toText('送给你'))
+        promise.then(() => engine.toText(''))
         .then(() => engine.shake())
 	    .then(() => engine.toText('L'))
         .then(() => engine.shake())
@@ -36,7 +20,7 @@
         .then(() => engine.shake())
 	    .then(() => engine.toText('E'))
         .then(() => engine.shake())
-	    .then(() => engine.toText('笑一笑~'))
+	    .then(() => engine.toText(''))
         .then(() => engine.shake())
         .then(() => engine.clear())
         .then(() => document.getElementById('go').addEventListener('click', start));
